aeeb7a1ce21e5d00ef3ac157741812cf82095d82
berkgulsen <bipurpie.exe@gmail.com>
2023-10-17 22:33:19 +0300

    Crypto model created

---
+++ CryptoCrazyRxSwiftMVVM/Model/Crypto.swift
@@ -0,0 +1,13 @@
+//
+//  Crypto.swift
+//  CryptoCrazyRxSwiftMVVM
+//
+//  Created by Berk Gülşen on 17.10.2023.
+//
+
+import Foundation
+
+struct Crypto : Codable {
+    let currency: String
+    let price: String
+}
